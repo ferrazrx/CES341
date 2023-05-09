23010a47493c785d2f16805784e87746e71b2d6b
Rafael Ferraz <rafael_ferraz@apple.com>
2023-05-09 10:08:33 -0300

    update cors swagger

--- Week4/swagger.js
+++ Week4/swagger.js
@@ -10,7 +10,7 @@ const doc = {
   },
   consumes: ['application/json'],
   produces: ['application/json'],
-  host: 'https://week4-3tit.onrender.com/',
+  host: 'week4-3tit.onrender.com/',
   schemes: ['http'],
   basePath: '/contacts/',
 };
